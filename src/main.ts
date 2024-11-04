c32456e72afc1e0ac974e8f3703e4f97789f2e9d
JC Zaragoza <jzara3115@gmail.com>
2024-11-04 13:58:19 -0800

    Setup - added button

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-// todo
+const button = document.createElement("button");
+button.textContent = "Click me";
+
+button.addEventListener("click", () => {
+  alert("You clicked the button!");
+});
+
+document.body.appendChild(button);
